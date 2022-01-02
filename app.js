d3f7231cded42b9858f3607c475979ba3e85fee9
Mario Halim <mariohalim00@gmail.com>
2022-01-02 21:35:34 +0700

    Mario Halim

---
+++ app.js
@@ -0,0 +1,194 @@
+const mydata = {
+  password_management: [
+    {
+      name: "Spectre",
+      link: "https://spectre.app",
+    },
+    {
+      name: "BitWarden",
+      link: "https://vault.bitwarden.com/#/",
+    },
+    {
+      name: "LessPass",
+      link: "https://www.lesspass.com/#/",
+    },
+  ],
+  utilities: [
+    {
+      name: "HackMD",
+      link: "https://hackmd.io/",
+    },
+    {
+      name: "Overleaf",
+      link: "https://www.overleaf.com",
+    },
+    {
+      name: "Privacy Tools",
+      link: "https://www.privacytools.io",
+    },
+    {
+      name: "Linux Documentation",
+      link: "https://linux.die.net",
+    },
+  ],
+
+  entertainment: [
+    {
+      name: "Netflix",
+      link: "https://www.netflix.com/browse",
+    },
+    {
+      name: "Gogoanime",
+      link: "https://ww2.gogoanimes.org",
+    },
+    {
+      name: "YouTube",
+      link: "https://www.youtube.com",
+    },
+    {
+      name: "r/unixporn",
+      link: "https://www.reddit.com/r/unixporn/",
+    },
+  ],
+  social: [
+    {
+      name: "Discord",
+      link: "https://discord.com/login",
+    },
+    {
+      name: "Whatsapp Web",
+      link: "https://web.whatsapp.com",
+    },
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com",
+    },
+    {
+      name: "gmail",
+      link: "https://www.mail.google.com",
+    },
+    {canon
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/login?fromSignIn=true",
+    },
+  ],
+  academia: [
+    {
+      name: "BINUS Maya",
+      link: "https://binusmaya.binus.ac.id/newDefault/login.html",
+    },
+    {
+      name: "socs1 - thesis",
+      link: "https://socs1.binus.ac.id/proposalskripsi/auth/login",
+    },
+  ],
+  development: [
+    {
+      name: "GitHub",
+      link: "https://github.com",
+    },
+    {
+      name: "GitLab",
+      link: "https://gitlab.com/users/sign_in",
+    },
+    {
+      name: "Colab",
+      link: "https://socs1.binus.ac.id/proposalskripsi/auth/login",
+    },
+    {
+      name: "Kaggle",
+      link: "https://www.kaggle.com",
+    },
+  ],
+  shopping_and_travelling: [
+    {
+      name: "Shopee",
+      link: "https://shopee.co.id",
+    },
+    {
+      name: "Tokopedia",
+      link: "https://shopee.co.id",
+    },
+  ],
+  music_and_games: [
+    {
+      name: "Spotify",
+      link: "https://open.spotify.com",
+    },
+    {
+      name: "Steam",
+      link: "https://store.steampowered.com",
+    },
+    {
+      name: "Epic",
+      link: "https://www.epicgames.com/site/en-US/home",
+    },
+    {
+      name: "Hoyolab",
+      link: "https://www.hoyolab.com",
+    },
+  ],
+  miscellaneous: [
+    {
+      name: "Rizonrice - ricing wiki",
+      link: "https://rizonrice.club/Rice:Resources",
+    },
+    {
+      name: "Intro to ricing post",
+      link: "https://thatnixguy.github.io/posts/ricing/",
+    },
+    {
+      name: "LandChad",
+      link: "https://landchad.net/",
+    },
+    {
+      name: "Based Cooking",
+      link: "https://based.cooking/",
+    },
+    {
+      name: "my genshin app",
+      link: "#",
+      wip: true,
+    },
+    {
+      name: "Personal Webpage - WIP",
+      link: "#",
+      wip: true,
+    },
+  ],
+};
+
+const rootComponent = {
+  data() {
+    return {
+      text: "",
+      data: mydata,
+      img: {
+        password_management: "./image/passwords.gif",
+        utilities: "./image/utilities2.gif",
+        entertainment: "./image/entertainment.gif",
+        social: "./image/communication.gif",
+        academia: "./image/academia2.gif",
+        development: "./image/development2.gif",
+        music_and_games: "./image/music.gif",
+        video_and_photography: "./image/videoandphotography.gif",
+        shopping_and_travelling: "./image/shoppingandtravel.gif",
+        miscellaneous: "./image/misc.gif",
+      }
+    };
+  },
+  methods: {
+    search() {
+      const google_url = "https://www.google.com/search?q=";
+      const search_q = this.text;
+      const url = google_url + search_q;
+      window.open(url, "_blank");
+      win.focus();
+    },
+  },
+};
+
+const app = Vue.createApp(rootComponent);
+
+app.use(Quasar);
+app.mount("#app");
